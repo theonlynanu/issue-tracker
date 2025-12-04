2fa2a1b2b462bedc3d9c5240c2ea85c535a17828
Danyal Ahmed <danyalsmail@gmail.com>
2025-12-03 19:28:23 -0500

    Update default API port to 8000
    Did not know that MacOS reserves port 5000 for AirPlay
    Receiver.

--- backend/app.py
+++ backend/app.py
@@ -1777,4 +1777,4 @@ def create_app():
     
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port = 5000, debug=app.config["DEBUG"])
+    app.run(host="0.0.0.0", port = 8000, debug=app.config["DEBUG"])
